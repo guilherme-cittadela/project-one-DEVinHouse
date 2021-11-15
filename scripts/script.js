5ac9fd71a4de972995f6eb6d9a19b6554aafb53d
guilherme-cittadela <guilherme_cittadela@estudante.sesisenai.org.br>
2021-11-14 22:20:45 -0300

    Rename functions

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,8 @@ var _local_storage = localStorage.getItem('tasks'); //busca no localstorage os d
 
 //função que carrega os dados do local storage ao carregar a página
 window.onload = function(){
-    
     _local_storage = localStorage.getItem('tasks')
     arrayTasks = JSON.parse(_local_storage); 
-
     for(var i = 0; i < arrayTasks.tasks.length; i++ ){ //percorre o local storage e carrega os dados existentes
         //chama a funcção para criar o html para cada item salvo no localstorage
         createTaskList(arrayTasks.tasks[i])
@@ -21,24 +19,24 @@ window.onload = function(){
 //função para criar a lista de tarefas
 function createTaskList(element){
     var ul = document.getElementById('task-list'); 
-        var div = document.createElement('div')
-        div.className = 'tasks'
-        ul.appendChild(div)
-        var span = document.createElement('span')
-        var checkbox = document.createElement('input')
-        console.log(checkbox)
-        checkbox.type = 'checkbox'
-        checkbox.name = 'checktask'
-        checkbox.onchange = function(eve) {isChecked(eve, this)}
-        span.appendChild(checkbox)
-        span.appendChild(document.createTextNode(element))
-        div.appendChild(span);
-        var button = document.createElement('button');
-        button.innerText = "Excluir";
-        button.className = "btnDel";
-        button.setAttribute("id", element)
-        div.appendChild(button)
-        button.onclick = function() {deleteTask(this)};
+    var div = document.createElement('div')
+    div.className = 'tasks'
+    ul.appendChild(div)
+    var span = document.createElement('span')
+    var checkbox = document.createElement('input')
+    console.log(checkbox)
+    checkbox.type = 'checkbox'
+    checkbox.name = 'checktask'
+    checkbox.onchange = function(eve) {isChecked(eve, this)}
+    span.appendChild(checkbox)
+    span.appendChild(document.createTextNode(element))
+    div.appendChild(span);
+    var button = document.createElement('button');
+    button.innerText = "Excluir";
+    button.className = "btnDel";
+    button.setAttribute("id", element)
+    div.appendChild(button)
+    button.onclick = function() {deleteTask(this)};
 }
 
 
@@ -49,23 +47,24 @@ btnAdd.onclick = function(){
     var taskValue = document.getElementById('addTask');
     _local_storage = localStorage.getItem('tasks');
 
+
     if(taskValue.value === ""){    //validação do input, verifica se algo foi digitado
         alert('Adicione uma tarefa')
     }else{
-
+        
         if(_local_storage === null){
             var jsonTasks = '{"tasks" : ["' + taskValue.value + '"]}'
             var objeto = JSON.parse(jsonTasks); 
             var jsonTasks_local_storage = JSON.stringify(objeto)
             localStorage.setItem('tasks', jsonTasks_local_storage)
         } else{
-
             arrayTasks = JSON.parse(_local_storage);
             arrayTasks['tasks'].push(taskValue.value)  //adicionar valor a um array
             jsonTasks_local_storage = JSON.stringify(arrayTasks);
             localStorage.setItem('tasks', jsonTasks_local_storage)
-        
         }
+
+
         //chama a funcção para criar o html
         createTaskList(taskValue.value)
         taskValue.value = ''
@@ -78,9 +77,9 @@ btnAdd.onclick = function(){
 function isChecked(eve, element) {
     if(eve.currentTarget.checked){
         element.parentElement.style["text-decoration"] = 'line-through';
-        
+    } else{
+        element.parentElement.style["text-decoration"] = 'none';
     }
-    else{element.parentElement.style["text-decoration"] = 'none';}
 }
 
 
@@ -88,20 +87,15 @@ function isChecked(eve, element) {
 //deleta determinado item do localstorage
 function deleteTask(elemento){
     if(window.confirm("Você deseja excluir a tarefa?")){
-
         _local_storage = localStorage.getItem('tasks')
         var objeto = JSON.parse(_local_storage);
         arrayTasks = objeto.tasks
-
         var resultado = arrayTasks.filter(valor => valor !== elemento.id) 
-
         objeto.tasks = resultado
         _local_storage = JSON.stringify(objeto)
         localStorage.setItem('tasks', _local_storage)
-
         elemento.parentElement.remove()
     }
-
 }
 
 
